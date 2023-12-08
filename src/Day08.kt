59a532a98c5aaf44b4d8a5115a84430796b8adcb
Sergei Ovsiannikov <seregao@mail.ru>
2023-12-08 10:22:00 +0100

    Day08

---
+++ src/Day08.kt
@@ -0,0 +1,153 @@
+fun main() {
+
+    /*
+--- Day 8: Haunted Wasteland ---
+
+You're still riding a camel across Desert Island when you spot a sandstorm quickly approaching. When you turn to warn the Elf, she disappears before your eyes! To be fair, she had just finished warning you about ghosts a few minutes ago.
+
+One of the camel's pouches is labeled "maps" - sure enough, it's full of documents (your puzzle input) about how to navigate the desert. At least, you're pretty sure that's what they are; one of the documents contains a list of left/right instructions, and the rest of the documents seem to describe some kind of network of labeled nodes.
+
+It seems like you're meant to use the left/right instructions to navigate the network. Perhaps if you have the camel follow the same instructions, you can escape the haunted wasteland!
+
+After examining the maps for a bit, two nodes stick out: AAA and ZZZ. You feel like AAA is where you are now, and you have to follow the left/right instructions until you reach ZZZ.
+
+This format defines each node of the network individually. For example:
+
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
+Starting with AAA, you need to look up the next element based on the next left/right instruction in your input. In this example, start with AAA and go right (R) by choosing the right element of AAA, CCC. Then, L means to choose the left element of CCC, ZZZ. By following the left/right instructions, you reach ZZZ in 2 steps.
+
+Of course, you might not find ZZZ right away. If you run out of left/right instructions, repeat the whole sequence of instructions as necessary: RL really means RLRLRLRLRLRLRLRL... and so on. For example, here is a situation that takes 6 steps to reach ZZZ:
+
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+Starting at AAA, follow the left/right instructions. How many steps are required to reach ZZZ?
+
+Your puzzle answer was 16579.
+
+--- Part Two ---
+
+The sandstorm is upon you and you aren't any closer to escaping the wasteland. You had the camel follow the instructions, but you've barely left your starting position. It's going to take significantly more steps to escape!
+
+What if the map isn't for people - what if the map is for ghosts? Are ghosts even bound by the laws of spacetime? Only one way to find out.
+
+After examining the maps a bit longer, your attention is drawn to a curious fact: the number of nodes with names ending in A is equal to the number ending in Z! If you were a ghost, you'd probably just start at every node that ends with A and follow all of the paths at the same time until they all simultaneously end up at nodes that end with Z.
+
+For example:
+
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+Here, there are two starting nodes, 11A and 22A (because they both end with A). As you follow each left/right instruction, use that instruction to simultaneously navigate away from both nodes you're currently on. Repeat this process until all of the nodes you're currently on end with Z. (If only some of the nodes you're on end with Z, they act like any other node and you continue as normal.) In this example, you would proceed as follows:
+
+Step 0: You are at 11A and 22A.
+Step 1: You choose all of the left paths, leading you to 11B and 22B.
+Step 2: You choose all of the right paths, leading you to 11Z and 22C.
+Step 3: You choose all of the left paths, leading you to 11B and 22Z.
+Step 4: You choose all of the right paths, leading you to 11Z and 22B.
+Step 5: You choose all of the left paths, leading you to 11B and 22C.
+Step 6: You choose all of the right paths, leading you to 11Z and 22Z.
+So, in this example, you end up entirely on nodes that end in Z after 6 steps.
+
+Simultaneously start on every node that ends with A. How many steps does it take before you're only on nodes that end with Z?
+
+Your puzzle answer was 12927600769609.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+     */
+    fun parsePaths(input: List<String>): Map<String, Map<Char, String>> {
+        val paths = input.drop(2).associate {
+            val (from, to) = it.split(" = ")
+            val (_, left, right) = to.split("(", ")", ", ")
+            // debug traps:
+            // if(left == right && left == from) it.println()
+            from to mapOf('L' to left, 'R' to right)
+        }
+        return paths
+    }
+
+    fun part1(input: List<String>): Int {
+        val instructions = input[0]
+        val paths = parsePaths(input)
+
+        var cnt = 0
+        var position = "AAA"
+        while (position != "ZZZ") {
+            val step = instructions[cnt % instructions.length]
+            position = paths[position]!![step]!!
+            cnt++
+        }
+        return cnt
+    }
+
+    // part2
+
+    fun findLCM(a: Long, b: Long): Long {
+        val larger = if (a > b) a else b
+        val maxLcm = a * b
+        var lcm = larger
+        while (lcm <= maxLcm) {
+            if (lcm % a == 0L && lcm % b == 0L) {
+                return lcm
+            }
+            lcm += larger
+        }
+        return maxLcm
+    }
+
+    fun findLCMOfListOfNumbers(numbers: List<Long>): Long {
+        var result = numbers[0]
+        for (i in 1 ..<numbers.size) {
+            result = findLCM(result, numbers[i])
+        }
+        return result
+    }
+
+    fun part2(input: List<String>): Long {
+        val instructions = input[0]
+        val paths = parsePaths(input)
+
+        val positions = paths.keys.filter { it.endsWith("A") }
+            .map {
+                var cnt = 0L
+                var p = it
+                while (!p.endsWith('Z')) {
+                    val step = instructions[(cnt % instructions.length).toInt()]
+                    p = paths[p]!![step]!!
+                    cnt++
+                }
+                cnt
+            }
+        positions.println()
+        return findLCMOfListOfNumbers(positions)
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput1 = readInput("Day08_test1")
+    val testInput2 = readInput("Day08_test2")
+    val testInput3 = readInput("Day08_test3")
+    check(part1(testInput1) == 2)
+    check(part1(testInput2) == 6)
+    check(part2(testInput3) == 6L)
+
+    val input = readInput("Day08")
+    part1(input).println() // 16579
+    part2(input).println() // 12927600769609
+}
